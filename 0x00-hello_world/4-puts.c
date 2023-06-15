a6e32fb4bfe1305d860128d21d26ee4d32afb08d
YT-ANZU <yassinsghiouare@gmail.com>
2023-06-15 15:07:27 -0700

    pls

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,16 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a newline."
- * return 0.
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return(0);
 }
