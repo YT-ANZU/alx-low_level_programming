f1f60158a197683e283eb3ab351e1d874789537a
YT-ANZU <yassinsghiouare@gmail.com>
2023-06-15 19:25:14 -0700

    pls

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
