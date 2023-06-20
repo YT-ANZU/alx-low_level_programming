c7d5ebf715bb774bba73f983dd36d9196c73b2ae
YT-ANZU <yassinsghiouare@gmail.com>
2023-06-20 13:28:40 -0700

    pls

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
-#include "holberton.h"
+#include "main.h"
+
+
 /**
- * print_alphabet - wirte the the lower-case alphabet on a line.
- *
- *
- *
+ * print_alphabet - print all alphabet in lowercase
  */
+
+
 void print_alphabet(void)
 {
-	char c;
+		char letter;
 
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
 }
